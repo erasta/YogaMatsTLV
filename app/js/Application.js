c3fc8dd995d1f8888258d6724a0bf21855821643
erasta <eran.geva@gmail.com>
2017-06-12 15:14:17 +0300

    arrow

--- app/js/Application.js
+++ app/js/Application.js
@@ -2,13 +2,13 @@ class Application {
     init() {
         // instantiate a loader
         let e = document.getElementById('cityImage');
-        let tex = new THREE.Texture( e );
+        let tex = new THREE.Texture(e);
         tex.needsUpdate = true;
         tex.minFilter = THREE.LinearFilter;
         // debugger;
         this.lineMaterial = new THREE.LineBasicMaterial({ color: 'black', linewidth: 1 });
         this.imageMaterial = new THREE.MeshBasicMaterial({ map: tex, side: THREE.DoubleSide });
-        this.dotMaterial = new THREE.MeshStandardMaterial({color: 'red'});
+        this.dotMaterial = new THREE.MeshStandardMaterial({ color: 'red' });
         this.initGui();
         this.applyGuiChanges();
     }
@@ -34,14 +34,11 @@ class Application {
     }
 
     onMouseMove(inter) {
-        this.sceneManager.scene.remove(this.dot);
-
         let e = document.getElementById('matLabel');
+        if (this.dot) this.dot.visible = false;
+        e.textContent = '----';
         inter = inter.filter(t => t.object === this.plane);
-        if (inter.length === 0) {
-            e.textContent = 'None';
-            return;
-        }
+        if (inter.length === 0) return;
 
         const point = inter[0].point;
         const xmat = Math.ceil((point.x + this.width / 2) / this.matWidth);
@@ -50,7 +47,19 @@ class Application {
 
         const xpos = (xmat - 0.5) * this.matWidth - this.width / 2;
         const ypos = (ymat - 0.5) * this.matHeight - this.height / 2;
-        this.sceneManager.scene.add(this.dot = new THREE.Mesh(new THREE.SphereGeometry(0.1), this.dotMaterial));
+        if (!this.dot) {
+            this.sceneManager.scene.add(this.dot = new THREE.Object3D());
+            // this.dot.add(new THREE.Mesh(new THREE.SphereGeometry(0.1), this.dotMaterial));
+            const cone = new THREE.Mesh(new THREE.CylinderGeometry(0.25, 0, 0.5, 8, 1, false), this.dotMaterial);
+            cone.rotation.x = Math.PI / 2;
+            cone.position.z = 0.25;
+            const cyl = new THREE.Mesh(new THREE.CylinderGeometry(0.125, 0.125, 0.5, 8, 1, false), this.dotMaterial);
+            cyl.rotation.x = Math.PI / 2;
+            cyl.position.z = 0.75;
+            this.dot.add(cone);
+            this.dot.add(cyl);
+        }
+        this.dot.visible = true;
         this.dot.position.set(xpos, ypos, 0);
     }
 
